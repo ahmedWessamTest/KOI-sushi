93bbd18bab1e52d3c7370d53c6ee5c2198e401c4
ahmedWessamTest <ahmedwessam202@gmail.com>
2026-02-03 12:31:38 +0200

    product interface

--- src/app/pages/dashboard/dashboard-pages/c-dashboard-menu/b-products/b-products-details/b-products-details.component.ts
+++ src/app/pages/dashboard/dashboard-pages/c-dashboard-menu/b-products/b-products-details/b-products-details.component.ts
@@ -4,9 +4,32 @@ import { ActivatedRoute, RouterLink } from "@angular/router";
 import { ButtonModule } from "primeng/button";
 import { CardModule } from "primeng/card";
 import { TagModule } from "primeng/tag";
-import { IGetProductById } from "../../../../../../core/Interfaces/d-products/IGetProductById";
 import {  WEB_SITE_IMG_URL } from "../../../../../../core/constants/WEB_SITE_BASE_UTL";
+interface ProductData {
+  id: number
+  category_id: number
+  title_ar: string
+  title_en: string
+  description_ar: string
+  description_en: string
+  price: string
+  main_image: string
+  total_orders: number
+  status: boolean
+  created_at: string
+  updated_at: string
+  has_options: boolean
+  is_recommended: boolean
+  images: Image[]
+}
 
+interface Image {
+  id: number
+  product_id: number
+  image: string
+  created_at: string
+  updated_at: string
+}
 @Component({
   selector: "app-b-products-details",
   standalone: true,
@@ -17,7 +40,7 @@ import {  WEB_SITE_IMG_URL } from "../../../../../../core/constants/WEB_SITE_BAS
 export class BProductsDetailsComponent {
   private ActivatedRoute = inject(ActivatedRoute);
   readonly imgUrl = WEB_SITE_IMG_URL;
-  productData!: any;
+  productData!: ProductData;
 
   ngOnInit(): void {
     this.productData = this.ActivatedRoute.snapshot.data["products"].data;
